27aa6752a600292be5b0e7e14c0141f0aea6d20a
Aletz <pedrolop15@hotmail.com>
2017-10-06 18:06:42 -0500

    tipo de datp

--- image.py
+++ image.py
@@ -8,9 +8,22 @@ print "nombre del programa",__name__
 if __name__ == '__main__':
 	if len(sys.argv) > 1:
 		print sys.argv[1]
-		imagen = Image.open(sys.argv[1])
-		imagen.show()
-		print(imagen.format)   # JPEG
-		print(imagen.mode)   # Tipo de imagen: RGB, CMYK, L, P, etc.
+		try:
+			imagen = Image.open(sys.argv[1])
+			print "tipo dato Image.open :",type(imagen)
+			if imagen is not None:
+				if imagen.mode != "RGB":
+					imagen.show()
+					print(imagen.format)   # JPEG
+					print(imagen.mode)
+					print(imagen.size) 
+					pix = imagen.load()
+					print "RGB : ",pix[0,0] #Get the RGBA Value of the a pixel of an image
+				else:
+					print "Solo trabajo con imagenes en rgb" 
+		except Exception as e:
+			print "Error al abrir la imagen"
+		
+		  # Tipo de imagen: RGB, CMYK, L, P, etc.
 	else:
 		print "Uso image.py <nombreImagen>"
